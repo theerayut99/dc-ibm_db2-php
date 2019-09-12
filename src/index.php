d491b95078a16177b621ae37954bb1f579b0ec5a
Theerayut Thanyaphoo <theerayut.t@mono.co.th>
2019-09-12 10:47:36 +0700

    แก้ไข server hostname เป็น container

--- src/index.php
+++ src/index.php
@@ -2,41 +2,13 @@
     $value = "World";
     // echo phpinfo();
 
-    // $db = new PDO('mysql:host=database;dbname=mydb;charset=utf8mb4', 'myuser', 'secret');
-    
-    // $databaseTest = ($db->query('SELECT * FROM dockerSample'))->fetchAll(PDO::FETCH_OBJ);
-
-    // try {
-    //     $db = new PDO("odbc:DRIVER={IBM i Access ODBC Driver};DATABASE=testdb;HOSTNAME=db2;PORT=50000;PROTOCOL=TCPIP;", "db2inst1", "db2inst1");
-    //     echo "<pre>";
-    //     print_r($db);
-    //     exit;
-    // } catch (PDOException $e) {
-    //     echo $e->getMessage();
-    // }
-
-    // try { 
-    //     $connection = new PDO("ibm:testdb", "db2inst1", "db2inst1", array(
-    //         PDO::ATTR_PERSISTENT => TRUE, 
-    //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-    //     ); 
-    // }
-    // catch (Exception $e) {
-    //     echo($e->getMessage());
-    // }
-
-    // $database = 'testdb';
-    // $user = 'db2inst1';
-    // $password = 'db2inst1';
-    // $conn = db2_connect($database, $user, $password);
-
     $driver      = "DRIVER={IBM DB2 ODBC DRIVER};";
 
-    $database    = "testdb";         # Get these database details from
-    $hostname    = "localhost";   # the Connect page of the Db2 on Cloud
+    $database    = "testdb";        # Get these database details from
+    $hostname    = "db2";           # Container name
     $port        = 50000;           # web console.
-    $user        = "db2inst1";    #
-    $password    = "db2inst1";    #
+    $user        = "db2inst1";
+    $password    = "db2inst1";
     $dsn         = "DATABASE=$database;" .
                    "HOSTNAME=$hostname;" .
                    "PORT=$port;" .
