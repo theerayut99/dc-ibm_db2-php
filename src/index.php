6033a2d148770381a33cd8ba5c7b66fa8b5d70e2
Theerayut Thanyaphoo <theerayut.t@mono.co.th>
2019-09-12 16:33:57 +0700

    เพิ่ม CRUD table animals

--- src/index.php
+++ src/index.php
@@ -1,7 +1,4 @@
 <?php
-    $value = "World";
-    // echo phpinfo();
-
     $driver      = "DRIVER={IBM DB2 ODBC DRIVER};";
 
     $database    = "testdb";        # Get these database details from
@@ -22,21 +19,50 @@
 
     if ($conn) {
         echo "Connection succeeded.";
-        db2_close($conn);
     }
     else {
         echo "Connection failed: " . db2_conn_errormsg();
     }
-    
-    ?>
-    
-    <html>
-        <body>
-            <h1>Hello, <?= $value ?>!</h1>
-    
-            <!-- <?php foreach($databaseTest as $row): ?>
-                <p>Hello, <?= $row->name ?></p>
-            <?php endforeach; ?> -->
-        </body>
-    </html>
-    
+
+    // Create the test table
+    $create = 'CREATE TABLE animals (id INTEGER, breed VARCHAR(32), name CHAR(16), weight DECIMAL(7,2))';
+    $result = db2_exec($conn, $create);
+    if ($result) {
+        print "Successfully created the table.\n";
+    } else {
+        echo "Can not Create Table.";
+    }
+
+    // Populate the test table
+    // $animals = array(
+    //     array(1, 'แมว', 'ปุ๊ก', 3.2),
+    //     array(2, 'หมา', 'ปิ๊ก', 12.3),
+    //     array(3, 'ม้า', 'สมาร์ทตี้', 350.0),
+    //     array(4, 'ลา', 'สวีทเตอร์', 150)
+    // );
+
+    $animals = array(
+        array(0, 'cat', 'Pook', 3.2),
+        array(1, 'dog', 'Peaches', 12.3),
+        array(2, 'horse', 'Smarty', 350.0),
+        array(3, 'gold fish', 'Bubbles', 0.1),
+        array(4, 'budgerigar', 'Gizmo', 0.2),
+        array(5, 'goat', 'Rickety Ride', 9.7),
+        array(6, 'llama', 'Sweater', 150)
+    );
+
+    foreach ($animals as $animal) {
+        $rc = db2_exec($conn, "INSERT INTO animals (id, breed, name, weight) VALUES ({$animal[0]}, '{$animal[1]}', '{$animal[2]}', {$animal[3]})");
+        if ($rc) {
+            print "Insert... ";
+        }
+    }
+
+    $sql = "SELECT name FROM animals WHERE weight < 10.0 ORDER BY name";
+    if ($conn) {
+        $stmt = db2_exec($conn, $sql, array('cursor' => DB2_SCROLLABLE));
+        while ($row = db2_fetch_array($stmt)) {
+            echo "$row[0]\n<br>";
+        }
+    }
+    db2_close($conn);
